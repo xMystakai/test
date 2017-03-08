0a854ccbfbf8e95207f7692bb062e65de562dba6
kingsplatter <andrew.strange@nwktc.edu>
2017-03-08 10:40:16 -0700

    Added the Woot object

---
+++ File.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  GitPractice
+//
+//  Created by Andrew Strange on 3/8/17.
+//  Copyright © 2017 Kai Comer. All rights reserved.
+//
+
+import Foundation
+
+class Woot {
+    var stuff : String
+    var things : String
+    
+    init(stuff : String, andThings things : String) {
+        self.stuff = stuff
+        self.things = things
+    }
+}
